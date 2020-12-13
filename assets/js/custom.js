df83236eaa4a2f8c39af461e2bc3f2aacd1fa854
wissalrss <70477045+wissalrss@users.noreply.github.com>
2020-12-13 22:04:38 +0100

    some chnges

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,17 @@
-/*
 $(window).scroll(function(){
     if ($(window).scrollTop() >= 300) {
         $('nav').addClass('fixed-header');
-
     }
     else {
         $('nav').removeClass('fixed-header');
 
     }
-});*/
+});
+$(document).ready(function() {
+    var url = window.location;
+    var element = $('.nav-link').filter(function() {
+        return this.href == url || url.href.indexOf(this.href) == 0; }).addClass('active');
+    if (element.is('li')) {
+        element.addClass('active').parent().parent('li').addClass('active')
+    }
+});
